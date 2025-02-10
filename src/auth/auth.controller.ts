da7352bee692b3cf474fdbf28587eda04523a35e
JungMin <wjdals0304@gmail.com>
2025-02-10 10:01:22 +0900

    feat: 로그인 API 추가 및 회원가입 API jwt 로직 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,26 @@
-import { Controller, Post, Body, Req, UseGuards, Get } from '@nestjs/common';
+import { Controller, Post, Body, Req, UseGuards, Get, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from '../dto/create-user.dto';
+import { CreateUserDto, LoginDto } from '../dto/user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup/email')
-  async signUpWithEmail(@Body() createUserDto: CreateUserDto) {
-    return this.authService.signUpWithEmail(createUserDto);
+  async signUpWithEmail(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    return this.authService.signUpWithEmail(createUserDto, res);
   }
 
   @Get('signup/google')
   @UseGuards(AuthGuard('google'))
   async signUpWithGoogle(@Req() req) {
     return this.authService.signUpWithGoogle(req.user);
+  }
+
+  @Post('signin/email')
+  async signInWithEmail(@Body() loginDto: LoginDto, @Res() res: Response) {
+    return this.authService.signInWithEmail(loginDto, res);
   }
 } 
