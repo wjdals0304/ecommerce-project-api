97b066f5656d0caa275804a18813dfadc69b0181
JungMin <wjdals0304@gmail.com>
2025-03-26 10:24:07 +0900

    feat: vercel 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
   try {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(server),
+    );
+    
     app.enableCors({
-      origin: ['https://ecommerce-project-liart-one.vercel.app/', 'http://localhost:3000'],
+      origin: [
+        'https://ecommerce-project-liart-one.vercel.app',
+        'http://localhost:3000'
+      ],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       credentials: true,
       allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
@@ -20,12 +31,19 @@ async function bootstrap() {
       }),
     );
 
-    const port = process.env.PORT || 3001;
-    await app.listen(port);
-    console.log(`Application is running on port ${port}`);
+    await app.init();
+    return app.getHttpAdapter().getInstance();
   } catch (error) {
     console.error('Failed to start application:', error);
     process.exit(1);
   }
 }
-bootstrap();
+
+let cachedServer: any;
+
+export default async function handler(req: any, res: any) {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  return cachedServer(req, res);
+}
